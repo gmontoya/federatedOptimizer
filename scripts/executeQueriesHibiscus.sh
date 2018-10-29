6dd1b00e054d53d73b1e86694b3f5f885422c261
gmontoya <gmontoya@users.noreply.github.com>
2018-10-29 14:12:32 +0100

    Update executeQueriesHibiscus.sh
    Minor changes, introducing variables ODYSSEY_HOME, FEDX_HIBISCUS_HOME and configFileHibiscus to make easier the changes to run using other configurations

--- scripts/executeQueriesHibiscus.sh
+++ scripts/executeQueriesHibiscus.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-folder=/home/roott/queries/fedBench
-folder=/home/roott/tmp/views/size7
-folder=/home/roott/queries/complexQueries
-#cp /home/roott/fedBenchFederationVirtuoso.ttl /home/roott/fedBenchFederation.ttl
+fedBench=/home/roott/queries/fedBench
+ODYSSEY_HOME=/home/roott/federatedOptimizer
+FEDX_HIBISCUS_HOME=/home/roott/FedX-HiBISCus
+configFileHibiscus=${FEDX_HIBISCUS_HOME}/config.properties
 cold=true
 s=`seq 1 11`
 l=""
-n=1
+n=10
 w=1800
 for i in ${s}; do
     l="${l} LD${i}"
@@ -23,37 +23,17 @@ for i in ${s}; do
     l="${l} LS${i}"
 done
 
-l=""
-s=`seq 1 10`
-
-for i in ${s}; do
-    l="${l} C${i}"
-done
-
-
-#l="LS7"
-#l="6D7 LD11 CD6 CD7 LS5"
-
-#l=""
-
-#s=`seq 0 4`
-
-#for i in ${s}; do
-#    l="${l} fragment${i}"
-#done
-
 for query in ${l}; do
     f=0
-    cold=true
     for j in `seq 1 ${n}`; do
-        cd /home/roott/FedX-HiBISCus
-        if [ -f /home/roott/FedX-HiBISCus/summaries*?/* ]; then
-            rm /home/roott/FedX-HiBISCus/summaries*?/*
+        cd ${FEDX_HIBISCUS_HOME}
+        if [ -f ${FEDX_HIBISCUS_HOME}/summaries*?/* ]; then
+            rm ${FEDX_HIBISCUS_HOME}/summaries*?/*
         fi
         if [ "$cold" = "true" ] && [ -f cache.db ]; then
             rm cache.db
         fi
-        /usr/bin/time -f "%e %P %t %M" timeout ${w}s ./cli.sh -c config.properties @q ${folder}/${query} > outputFile 2> timeFile
+        /usr/bin/time -f "%e %P %t %M" timeout ${w}s ./cli.sh -c ${configFileHibiscus} @q ${fedBench}/${query} > outputFile 2> timeFile
 
         x=`grep "planning=" outputFile`
         y=`echo ${x##*planning=}`
@@ -78,57 +58,12 @@ for query in ${l}; do
             nr=`echo ${x##*results=}`
         fi
 
-        /home/roott/federatedOptimizer/scripts/processFedXPlansNSS.sh outputFile > xxx
+        ${ODYSSEY_HOME}/scripts/processFedXPlansNSS.sh outputFile > xxx
         nss=`cat xxx`
-        /home/roott/federatedOptimizer/scripts/processFedXPlansNSQ.sh outputFile > xxx
+        ${ODYSSEY_HOME}/scripts/processFedXPlansNSQ.sh outputFile > xxx
         ns=`cat xxx`
         rm xxx
-        echo "${query} ${nss} ${ns} ${s} ${t} ${nr}" >> outputExecuteQueriesHibiscusColdCacheB
-
-        if [ "$f" -ge "2" ]; then
-            break
-        fi
-    done
-    f=0
-    cold=false
-    for j in `seq 1 ${n}`; do
-        cd /home/roott/FedX-HiBISCus
-        if [ -f /home/roott/FedX-HiBISCus/summaries*?/* ]; then
-            rm /home/roott/FedX-HiBISCus/summaries*?/*
-        fi
-        if [ "$cold" = "true" ] && [ -f cache.db ]; then
-            rm cache.db
-        fi
-        /usr/bin/time -f "%e %P %t %M" timeout ${w}s ./cli.sh -c config.properties @q ${folder}/${query} > outputFile 2> timeFile
-        x=`grep "planning=" outputFile`
-        y=`echo ${x##*planning=}`
-        if [ -n "$y" ]; then
-            s=`echo ${y%%ms*}`
-        else
-            s=-1
-        fi
-        x=`tail -n 1 timeFile`
-        y=`echo ${x%% *}`
-        x=`echo ${y%%.*}`
-        if [ "$x" -ge "$w" ]; then
-            t=`echo $y`
-            t=`echo "scale=2; $t*1000" | bc`
-            f=$(($f+1))
-            nr=`grep "^\[" outputFile | grep "\]$" | wc -l | sed 's/^[ ^t]*//' | cut -d' ' -f1`
-        else
-            x=`grep "duration=" outputFile`
-            y=`echo ${x##*duration=}`
-            t=`echo ${y%%ms*}`
-            x=`grep "results=" outputFile`
-            nr=`echo ${x##*results=}`
-        fi
-
-        /home/roott/federatedOptimizer/scripts/processFedXPlansNSS.sh outputFile > xxx
-        nss=`cat xxx`
-        /home/roott/federatedOptimizer/scripts/processFedXPlansNSQ.sh outputFile > xxx
-        ns=`cat xxx`
-        rm xxx
-        echo "${query} ${nss} ${ns} ${s} ${t} ${nr}" >> outputExecuteQueriesHibiscusWarmCacheB
+        echo "${query} ${nss} ${ns} ${s} ${t} ${nr}" >> outputExecuteQueriesHibiscus
 
         if [ "$f" -ge "2" ]; then
             break
