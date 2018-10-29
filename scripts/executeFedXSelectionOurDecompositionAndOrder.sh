520599e793f01d304bda12416c36b71566587aef
gmontoya <gmontoya@users.noreply.github.com>
2018-10-29 12:56:25 +0100

    Update executeFedXSelectionOurDecompositionAndOrder.sh
    Minor changes, introducing variables ODYSSEY_HOME, JENA_HOME, and FedBenchDataPath to make easier the changes to run using other configurations

--- scripts/executeFedXSelectionOurDecompositionAndOrder.sh
+++ scripts/executeFedXSelectionOurDecompositionAndOrder.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-sed -i "s,optimize=.*,optimize=false," /home/roott/federatedOptimizer/lib/fedX3.1/config2
-#cp /home/roott/fedBenchFederationFuseki.ttl /home/roott/fedBenchFederation.ttl
-cp /home/roott/fedBenchFederationVirtuoso.ttl /home/roott/fedBenchFederation.ttl
+
+ODYSSEY_HOME=/home/roott/federatedOptimizer
+JENA_HOME=/home/roott/apache-jena-2.13.0
+fedBenchDataPath=/home/roott/fedbBenchData
+sed -i "s,optimize=.*,optimize=false," ${ODYSSEY_HOME}/lib/fedX3.1/config2
 fedBench=/home/roott/queries/fedBench
-newQueries=/home/roott/queries/fedBench_1_1
-#fedBench=/home/roott/queries/complexQueries
-#newQueries=/home/roott/queries/complexQueries_1_1
-generalPreds=/home/roott/generalPredicates
-#datasets=/home/roott/datasetsFuseki
+#newQueries=/home/roott/queries/fedBench_1_1
 datasets=/home/roott/datasetsVirtuoso
 cold=true
+
 s=`seq 1 11`
 l=""
 n=10
@@ -29,24 +28,14 @@ for i in ${s}; do
     l="${l} LS${i}"
 done
 
-#s=`seq 1 10`
-#l=""
-
-#for i in ${s}; do
-#    l="${l} C${i}"
-#done
-
-#l="LD6"
-#l="LD3 LD7 LD11 LS3 LS5 LS6 LS7"
-
 for query in ${l}; do
     f=0
     for j in `seq 1 ${n}`; do
-        cd /home/roott/federatedOptimizer/code
-        if [ "$cold" = "true" ] && [ -f /home/roott/federatedOptimizer/lib/fedX3.1/cache.db ]; then
-            rm /home/roott/federatedOptimizer/lib/fedX3.1/cache.db
+        cd ${ODYSSEY_HOME}/code
+        if [ "$cold" = "true" ] && [ -f ${ODYSSEY_HOME}/lib/fedX3.1/cache.db ]; then
+            rm ${ODYSSEY_HOME}/lib/fedX3.1/cache.db
         fi
-        /usr/bin/time -f "%e %P %t %M" timeout ${w}s java -cp .:/home/roott/apache-jena-2.13.0/lib/*:/home/roott/federatedOptimizer/lib/fedX3.1/lib/* evaluateFedXSelection $fedBench/$query ${datasets} /home/roott/fedBenchData 100000000 true false $newQueries/$query $generalPreds > outputFile 2> timeFile
+        /usr/bin/time -f "%e %P %t %M" timeout ${w}s java -cp .:${JENA_HOME}/lib/*:${ODYSSEY_HOME}/lib/fedX3.1/lib/* evaluateFedXSelection $fedBench/$query ${datasets} ${fedBenchDataPath} 100000000 true false $newQueries/$query > outputFile 2> timeFile
         x=`grep "decomposition=" outputFile`
         y=`echo ${x##*decomposition=}`
 
