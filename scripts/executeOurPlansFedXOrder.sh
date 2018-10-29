a7c1d3db662ffc1777c07319517a2eada1e795bc
gmontoya <gmontoya@users.noreply.github.com>
2018-10-29 13:30:25 +0100

    Update executeOurPlansFedXOrder.sh
    Minor changes, introducing variables ODYSSEY_HOME, JENA_HOME, proxyFederationFile, and FedBenchDataPath to make easier the changes to run using other configurations

--- scripts/executeOurPlansFedXOrder.sh
+++ scripts/executeOurPlansFedXOrder.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-sed -i "s,optimize=.*,optimize=false," /home/roott/federatedOptimizer/lib/fedX3.1/config2
-#cp /home/roott/fedBenchFederationFuseki.ttl /home/roott/fedBenchFederation.ttl
-cp /home/roott/fedBenchFederationVirtuoso.ttl /home/roott/fedBenchFederation.ttl
-#cp /home/roott/fedBenchFederationProxy.ttl /home/roott/fedBenchFederation.ttl
+ODYSSEY_HOME=/home/roott/federatedOptimizer
+JENA_HOME=/home/roott/apache-jena-2.13.0
+fedBenchDataPath=/home/roott/fedbBenchData
+sed -i "s,optimize=.*,optimize=false," ${ODYSSEY_HOME}/lib/fedX3.1/config2
 fedBench=/home/roott/queries/fedBench
-newQueries=/home/roott/queries/fedBench_1_1
-#fedBench=/home/roott/queries/complexQueries
-#newQueries=/home/roott/queries/complexQueries_1_1
-generalPreds=/home/roott/generalPredicates
-#datasets=/home/roott/datasetsFuseki
+#newQueries=/home/roott/queries/fedBench_1_1
 datasets=/home/roott/datasetsVirtuoso
-#datasets=/home/roott/datasets
 cold=true
+#proxyFederationFile=/home/roott/tmp/proxyFederation
+
 s=`seq 1 11`
 l=""
-n=1
+n=10
 w=1800
 for i in ${s}; do
     l="${l} LD${i}"
@@ -31,31 +28,20 @@ for i in ${s}; do
     l="${l} LS${i}"
 done
 
-#s=`seq 1 10`
-#l=""
-
-#for i in ${s}; do
-#    l="${l} C${i}"
-#done
-
-#l="LD7 LD11 CD6 CD7 LS5"
-#l="LD3 LD7 LD11 LS3 LS5 LS7"
-#l="CD7"
-
 for query in ${l}; do
     f=0
     for j in `seq 1 ${n}`; do
-        #cd /home/roott/federatedOptimizer/scripts
+        #cd ${ODYSSEY_HOME}/scripts
         #tmpFile=`./startProxies.sh 8891 8899 3030 "ChEBI KEGG Drugbank Geonames DBpedia Jamendo NYTimes SWDF LMDB"`
         #sleep 1s
-        #cd /home/roott/federatedOptimizer/scripts
+        #cd ${ODYSSEY_HOME}/scripts
         #./startFederation.sh > outputStartFederation
         #sleep 1m
-        cd /home/roott/federatedOptimizer/code
-        if [ "$cold" = "true" ] && [ -f /home/roott/federatedOptimizer/lib/fedX3.1/cache.db ]; then
-            rm /home/roott/federatedOptimizer/lib/fedX3.1/cache.db
+        cd ${ODYSSEY_HOME}/code
+        if [ "$cold" = "true" ] && [ -f ${ODYSSEY_HOME}/lib/fedX3.1/cache.db ]; then
+            rm ${ODYSSEY_HOME}/lib/fedX3.1/cache.db
         fi
-        /usr/bin/time -f "%e %P %t %M" timeout ${w}s java -cp .:/home/roott/apache-jena-2.13.0/lib/*:/home/roott/federatedOptimizer/lib/fedX3.1/lib/* evaluateOurPlansWithFedXOrder $fedBench/$query ${datasets} /home/roott/fedBenchData 100000000 true false $newQueries/$query $generalPreds > outputFile 2> timeFile
+        /usr/bin/time -f "%e %P %t %M" timeout ${w}s java -cp .:${JENA_HOME}/lib/*:${ODYSSEY_HOME}/lib/fedX3.1/lib/* evaluateOurPlansWithFedXOrder $fedBench/$query ${datasets} ${fedBenchDataPath} 100000000 true false $newQueries/$query > outputFile 2> timeFile
         x=`grep "planning=" outputFile`
         y=`echo ${x##*planning=}`
 
@@ -80,8 +66,8 @@ for query in ${l}; do
             x=`grep "results=" outputFile`
             nr=`echo ${x##*results=}`
         fi
-        #cd /home/roott/federatedOptimizer/scripts
-        #./killAll.sh /home/roott/tmp/proxyFederation
+        #cd ${ODYSSEY_HOME}/scripts
+        #./killAll.sh ${proxyFederationFile}
         #sleep 10s
         #pi=`./processProxyInfo.sh ${tmpFile} 0 8`
         echo "${query} ${d} ${t} ${nr}"
