59c071aceece6951a77507efe8ff9307c787def3
gmontoya <gmontoya@users.noreply.github.com>
2018-10-29 14:44:31 +0100

    Update executeQueriesSemaGrowWithProxies.sh
    Minor changes, introducing variables ODYSSEY_HOME, proxyFederationFile and SEMAGROW_HOME to make easier the changes to run using other configurations

--- scripts/executeQueriesSemaGrowWithProxies.sh
+++ scripts/executeQueriesSemaGrowWithProxies.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 folder=/home/roott/queries/fedBench
-#folder=/home/roott/queries/complexQueries
+SEMAGROW_HOME=/home/roott/semagrow
+ODYSSEY_HOME=/home/roott/federatedOptimizer
+proxyFederationFile=/home/roott/tmp/proxyFederation
+
 s=`seq 1 11`
 l=""
-n=1
+n=10
 w=1800
 for i in ${s}; do
     l="${l} LD${i}"
@@ -20,22 +23,14 @@ for i in ${s}; do
     l="${l} LS${i}"
 done
 
-#s=`seq 1 10`
-
-#for i in ${s}; do
-#    l="${l} C${i}"
-#done
-
-l="LS7"
-
 for query in ${l}; do
     f=0
     for j in `seq 1 ${n}`; do
-        cd /home/roott/federatedOptimizer/scripts
+        cd ${ODYSSEY_HOME}/scripts
         tmpFile=`./startProxies2.sh "172.19.2.123 172.19.2.106 172.19.2.100 172.19.2.115 172.19.2.107 172.19.2.118 172.19.2.111 172.19.2.113 172.19.2.120" 3030`
         #tmpFile=`./startProxies.sh 8891 8899 3030 "ChEBI KEGG Drugbank Geonames DBpedia Jamendo NYTimes SWDF LMDB"`
         sleep 2s
-        cd /home/roott/semagrow
+        cd ${SEMAGROW_HOME}
         queryStr=`tr "\n" " " < ${folder}/${query}`
         /usr/bin/time -f "%e %P %t %M" timeout ${w}s java -Xmx4096m -cp .:http/target/semagrow-http-1.5.1/WEB-INF/lib/* eu.semagrow.cli.CliMain /etc/default/semagrow/repository.ttl "${queryStr}" output.json > outputFile 2> timeFile
 
@@ -51,7 +46,7 @@ for query in ${l}; do
             t=`echo $y`
             t=`echo "scale=2; $t*1000" | bc`
             f=$(($f+1))
-            /home/roott/federatedOptimizer/scripts/fixJSONAnswer.sh output.json
+            ${ODYSSEY_HOME}/scripts/fixJSONAnswer.sh output.json
         else
             x=`grep "duration=" outputFile`
             y=`echo ${x##*duration=}`
@@ -64,16 +59,16 @@ for query in ${l}; do
         else
             s=-1
         fi
-        nr=`python /home/roott/federatedOptimizer/scripts/formatJSONFile.py output.json | wc -l | sed 's/^[ ^t]*//' | cut -d' ' -f1`
+        nr=`python ${ODYSSEY_HOME}/scripts/formatJSONFile.py output.json | wc -l | sed 's/^[ ^t]*//' | cut -d' ' -f1`
 
-        /home/roott/federatedOptimizer/scripts/processSemaGrowPlansNSS.sh outputFile > xxx
+        ${ODYSSEY_HOME}/scripts/processSemaGrowPlansNSS.sh outputFile > xxx
         nss=`cat xxx`
-        /home/roott/federatedOptimizer/scripts/processSemaGrowPlansNSQ.sh outputFile > xxx
+        ${ODYSSEY_HOME}/scripts/processSemaGrowPlansNSQ.sh outputFile > xxx
         ns=`cat xxx`
         rm xxx
 
-        cd /home/roott/federatedOptimizer/scripts
-        ./killAll.sh /home/roott/tmp/proxyFederation
+        cd ${ODYSSEY_HOME}/scripts
+        ./killAll.sh ${proxyFederationFile}
         sleep 10s
         pi=`./processProxyInfo.sh ${tmpFile} 0 8`
 
