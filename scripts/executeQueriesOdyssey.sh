b18bb91a3dbee2acbce2fe50c27146e82c3163fa
gmontoya <gmontoya@users.noreply.github.com>
2018-10-29 11:56:52 +0100

    Update executeQueriesOdyssey.sh
    Minor changes, introducing variables ODYSSEY_HOME, JENA_HOME and FedBenchDataPath to make easier the changes to run using other configurations

--- scripts/executeQueriesOdyssey.sh
+++ scripts/executeQueriesOdyssey.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
-sed -i "s,optimize=.*,optimize=false," /home/roott/federatedOptimizer/lib/fedX3.1/config2
-#cp /home/roott/fedBenchFederationFuseki.ttl /home/roott/fedBenchFederation.ttl
-#cp /home/roott/fedBenchFederationVirtuoso.ttl /home/roott/fedBenchFederation.ttl
-cp /home/roott/fedBenchLocalFederationVirtuoso.ttl /home/roott/fedBenchFederation.ttl
-#fedBench=/home/roott/tmp/views/size7
+ODYSSEY_HOME=/home/roott/federatedOptimizer
+JENA_HOME=/home/roott/apache-jena-2.13.0
+fedBenchDataPath=/home/roott/fedbBenchData
+
+sed -i "s,optimize=.*,optimize=false," ${ODYSSEY_HOME}/lib/fedX3.1/config2
 fedBench=/home/roott/queries/fedBench
-newQueries=/home/roott/queries/fedBench_1_1
-#fedBench=/home/roott/queries/complexQueries
-#newQueries=/home/roott/queries/complexQueries_1_1
-generalPreds=/home/roott/generalPredicates
-#datasets=/home/roott/datasetsFuseki
-#datasets=/home/roott/datasetsVirtuoso
-datasets=/home/roott/datasetsLocalVirtuoso
+#newQueries=/home/roott/queries/fedBench_1_1
+datasets=/home/roott/datasetsVirtuoso
 cold=true
+
 s=`seq 1 11`
 l=""
-n=1
+n=10
+
 w=1800
 for i in ${s}; do
     l="${l} LD${i}"
 done
 
-#s=`seq 1 7`
+s=`seq 1 7`
 
-#for i in ${s}; do
-#    l="${l} CD${i}"
-#done
+for i in ${s}; do
+    l="${l} CD${i}"
+done
 
-#for i in ${s}; do
-#    l="${l} LS${i}"
-#done
+for i in ${s}; do
+    l="${l} LS${i}"
+done
 
 #s=`seq 1 10`
 #l=""
@@ -39,26 +36,14 @@ done
 #    l="${l} C${i}"
 #done
 
-#l="LD7 LD11 CD6 CD7 LS5"
-#l="$1"
-
-#s=`seq 0 4`
-#s="1"
-#for i in ${s}; do
-#    l="${l} fragment${i}"
-#done
-
-#l="LS3"
-
 for query in ${l}; do
     f=0
     for j in `seq 1 ${n}`; do
-        cd /home/roott/federatedOptimizer/code
-        if [ "$cold" = "true" ] && [ -f /home/roott/federatedOptimizer/lib/fedX3.1/cache.db ]; then
-            rm /home/roott/federatedOptimizer/lib/fedX3.1/cache.db
+        cd ${ODYSSEY_HOME}/code
+        if [ "$cold" = "true" ] && [ -f ${ODYSSEY_HOME}/lib/fedX3.1/cache.db ]; then
+            rm ${ODYSSEY_HOME}/lib/fedX3.1/cache.db
         fi
-        /usr/bin/time -f "%e %P %t %M" timeout ${w}s java -Xmx4096m -cp .:/home/roott/apache-jena-2.13.0/lib/*:/home/roott/federatedOptimizer/lib/fedX3.1/lib/* evaluateSPARQLQuery $fedBench/$query ${datasets} /home/roott/fedbBenchData 100000000 true false $newQueries/$query $generalPreds > outputFile 2> timeFile
-        #/usr/bin/time -f "%e %P %t %M" timeout ${w}s java -cp .:/home/roott/apache-jena-2.13.0/lib/*:/home/roott/federatedOptimizer/lib/fedX3.1/lib/* evaluateSPARQLQuery $fedBench/$query ${datasets} /home/roott/fedBenchData 100000000 true false $newQueries/$query $generalPreds > outputFile 2> timeFile
+        /usr/bin/time -f "%e %P %t %M" timeout ${w}s java -Xmx4096m -cp .:${JENA_HOME}/lib/*:${ODYSSEY_HOME}/lib/fedX3.1/lib/* evaluateSPARQLQuery $fedBench/$query ${datasets} ${fedBenchDataPath} 100000000 true false $newQueries/$query > outputFile 2> timeFile
         x=`grep "planning=" outputFile`
         y=`echo ${x##*planning=}`
 
@@ -74,7 +59,7 @@ for query in ${l}; do
         if [ -n "$y" ]; then
             nss=`echo ${y}`
         else
-            /home/roott/federatedOptimizer/scripts/processFedXPlansNSS.sh outputFile > xxx
+            ${ODYSSEY_HOME}/scripts/processFedXPlansNSS.sh outputFile > xxx
             nss=`cat xxx`
             rm xxx
         fi
@@ -85,7 +70,7 @@ for query in ${l}; do
         if [ -n "$y" ]; then
             ns=`echo ${y}`
         else
-            /home/roott/federatedOptimizer/scripts/processFedXPlansNSQ.sh outputFile > xxx
+            ${ODYSSEY_HOME}/scripts/processFedXPlansNSQ.sh outputFile > xxx
             ns=`cat xxx`
             rm xxx
         fi
