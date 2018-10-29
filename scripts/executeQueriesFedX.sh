8d06f55b0cd4e296874c6a3d99826fe1cce6be5a
gmontoya <gmontoya@users.noreply.github.com>
2018-10-29 13:55:55 +0100

    Update executeQueriesFedX.sh
    Minor changes, introducing variables ODYSSEY_HOME and federationDescriptionFile to make easier the changes to run using other configurations

--- scripts/executeQueriesFedX.sh
+++ scripts/executeQueriesFedX.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-folder=/home/roott/queries/fedBench
-#folder=/home/roott/tmp/views/size7
-#folder=/home/roott/queries/complexQueries
-sed -i "s,optimize=.*,optimize=true," /home/roott/federatedOptimizer/lib/fedX3.1/config2
-#cp /home/roott/fedBenchFederationVirtuoso.ttl /home/roott/fedBenchFederation.ttl
-cp /home/roott/fedBenchLocalFederationVirtuoso.ttl /home/roott/fedBenchFederation.ttl
-#cp /home/roott/fedBenchFederationFuseki.ttl /home/roott/fedBenchFederation.ttl
+fedBench=/home/roott/queries/fedBench
+federationDescriptionFile=/home/roott/fedBenchFederation.ttl
+ODYSSEY_HOME=/home/roott/federatedOptimizer
+sed -i "s,optimize=.*,optimize=true," ${ODYSSEY_HOME}/lib/fedX3.1/config2
 cold=true
+
 s=`seq 1 11`
 l=""
-n=1
+n=10
 w=1800
 for i in ${s}; do
     l="${l} LD${i}"
@@ -26,33 +24,14 @@ for i in ${s}; do
     l="${l} LS${i}"
 done
 
-l=""
-s=`seq 1 2`
-
-for i in ${s}; do
-    l="${l} C${i}"
-done
-
-#l="LD3 LD11 CD3 CD7 LS3 LS4 LS5"
-#l="CD4 CD5 LS3 LS4 LS5"
-#l="LS3"
-
-l="LS2"
-
-#s=`seq 0 4`
-#s="4"
-#for i in ${s}; do
-#    l="${l} fragment${i}"
-#done
-
 for query in ${l}; do
     f=0
     for j in `seq 1 ${n}`; do
-        cd /home/roott/federatedOptimizer/lib/fedX3.1
+        cd ${ODYSSEY_HOME}/lib/fedX3.1
         if [ "$cold" = "true" ] && [ -f cache.db ]; then
             rm cache.db
         fi
-        /usr/bin/time -f "%e %P %t %M" timeout ${w}s ./cli.sh -c config2 -d /home/roott/fedBenchFederation.ttl @q ${folder}/${query} > outputFile 2> timeFile
+        /usr/bin/time -f "%e %P %t %M" timeout ${w}s ./cli.sh -c config2 -d ${federationDescriptionFile} @q ${folder}/${query} > outputFile 2> timeFile
 
         x=`grep "planning=" outputFile`
         y=`echo ${x##*planning=}`
@@ -76,9 +55,9 @@ for query in ${l}; do
             x=`grep "results=" outputFile`
             nr=`echo ${x##*results=}`
         fi
-        /home/roott/federatedOptimizer/scripts/processFedXPlansNSS.sh outputFile > xxx
+        ${ODYSSEY_HOME}/scripts/processFedXPlansNSS.sh outputFile > xxx
         nss=`cat xxx` 
-        /home/roott/federatedOptimizer/scripts/processFedXPlansNSQ.sh outputFile > xxx
+        ${ODYSSEY_HOME}/scripts/processFedXPlansNSQ.sh outputFile > xxx
         ns=`cat xxx`
         rm xxx
         echo "${query} ${nss} ${ns} ${s} ${t} ${nr}"
