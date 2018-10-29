f3d60064703bbcb9effa7f3957e2eb37ccf4374d
gmontoya <gmontoya@users.noreply.github.com>
2018-10-29 14:40:52 +0100

    Update executeQueriesSemaGrow.sh
    Minor changes, introducing variables ODYSSEY_HOME and SEMAGROW_HOME to make easier the changes to run using other configurations

--- scripts/executeQueriesSemaGrow.sh
+++ scripts/executeQueriesSemaGrow.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-folder=/home/roott/queries/fedBench
-#folder=/home/roott/queries/complexQueries
+fedBench=/home/roott/queries/fedBench
+SEMAGROW_HOME=/home/roott/semagrow
+ODYSSEY_HOME=/home/roott/federatedOptimizer
+
 s=`seq 1 11`
 l=""
-n=1
+n=10
 w=1800
 for i in ${s}; do
     l="${l} LD${i}"
@@ -20,45 +22,37 @@ for i in ${s}; do
     l="${l} LS${i}"
 done
 
-#s=`seq 1 10`
-
-#for i in ${s}; do
-#    l="${l} C${i}"
-#done
-
-l="LD1"
-
 for query in ${l}; do
     f=0
     for j in `seq 1 ${n}`; do
-        cd /home/roott/semagrow
+        cd ${SEMAGROW_HOME}
         queryStr=`tr "\n" " " < ${folder}/${query}`
-        /usr/bin/time -f "%e %P %t %M" timeout ${w}s java -Xmx4096m -cp .:http/target/semagrow-http-1.5.1/WEB-INF/lib/* eu.semagrow.cli.CliMain /etc/default/semagrow/repository.ttl "${queryStr}" outputOther.json > outputFileOther 2> timeFileOther
-        x=`tail -n 1 timeFileOther`
+        /usr/bin/time -f "%e %P %t %M" timeout ${w}s java -Xmx4096m -cp .:http/target/semagrow-http-1.5.1/WEB-INF/lib/* eu.semagrow.cli.CliMain /etc/default/semagrow/repository.ttl "${queryStr}" output.json > outputFile 2> timeFile
+        x=`tail -n 1 timeFile`
         y=`echo ${x%% *}`
         x=`echo ${y%%.*}`
         if [ "$x" -ge "$w" ]; then
             t=`echo $y`
             t=`echo "scale=2; $t*1000" | bc`
             f=$(($f+1))
-            /home/roott/federatedOptimizer/scripts/fixJSONAnswer.sh outputOther.json
+            ${ODYSSEY_HOME}/scripts/fixJSONAnswer.sh output.json
         else
-            x=`grep "duration=" outputFileOther`
+            x=`grep "duration=" outputFile`
             y=`echo ${x##*duration=}`
             t=`echo ${y%%ms*}`
         fi
-        x=`grep "planning=" outputFileOther`
+        x=`grep "planning=" outputFile`
         y=`echo ${x##*planning=}`
         if [ -n "$y" ]; then
             s=`echo ${y%%ms*}`
         else
             s=-1
         fi
-        nr=`python /home/roott/federatedOptimizer/scripts/formatJSONFile.py outputOther.json | wc -l | sed 's/^[ ^t]*//' | cut -d' ' -f1`
+        nr=`python ${ODYSSEY_HOME}/scripts/formatJSONFile.py output.json | wc -l | sed 's/^[ ^t]*//' | cut -d' ' -f1`
 
-        /home/roott/federatedOptimizer/scripts/processSemaGrowPlansNSS.sh outputFileOther > xxx
+        ${ODYSSEY_HOME}/scripts/processSemaGrowPlansNSS.sh outputFile > xxx
         nss=`cat xxx`
-        /home/roott/federatedOptimizer/scripts/processSemaGrowPlansNSQ.sh outputFileOther > xxx
+        ${ODYSSEY_HOME}/scripts/processSemaGrowPlansNSQ.sh outputFile > xxx
         ns=`cat xxx`
         rm xxx
         echo "${query} ${nss} ${ns} ${s} ${t} ${nr}"
