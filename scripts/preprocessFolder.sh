0144124076d4c357600d72f74ef0c7489173c336
gmontoya <gmontoya@cs.aau.dk>
2024-06-09 15:22:34 +0000

    adding a script to combine the content of a folder (files in .owl .rdf .nt .n3 formats) into one .nt file

---
+++ scripts/preprocessFolder.sh
@@ -0,0 +1,19 @@
+#preprocessFolder: combines the files in the folder into one file in N-Triple format
+. ./configFile
+
+pathToFiles=$1
+outputFile=$2
+
+okay="ntn3"
+
+for f in `ls $pathToFiles/*`; do
+    ext=`echo ${f##*\.}`
+    fileName=`echo ${f%\.*}`
+    x=`echo ${okay#*$ext}`
+    x=${#x}
+    if [ ${x} -lt ${#okay} ]; then
+        cat ${f} >> ${outputFile}
+    else 
+	    ${JENA_HOME}/bin/rdfparse ${fileName}.${ext} >> ${outputFile}
+    fi
+done
